af33d131828ec3099e19180692b86041c24dfa19
yeo-menghan <yeomenghan1989@gmail.com>
2024-05-10 20:31:19 +0800

    Clean up detectFraud.js and add comments

--- utils/detectFraud.js
+++ utils/detectFraud.js
@@ -1,6 +1,5 @@
 const { ChatOpenAI } = require("@langchain/openai");
 const { ChatPromptTemplate } = require("@langchain/core/prompts");
-const { StringOutputParser } = require("@langchain/core/output_parsers");
 const { JsonOutputFunctionsParser } = require("langchain/output_parsers");
 const fraudDetectionPrompt = require('../prompts/fraudDetectionPrompt');
 const researchPromptSplitJSON = require('../prompts/researchPromptSplitJSON');
@@ -34,37 +33,14 @@ const extractionFunctionSchema = {
 };
 
 /**
- * Detects fraud in email content using OpenAI's language model.
+ * Detects potential fraud in email content using OpenAI's language model.
+ *
  * @param {string} emailContent - The content of the email to analyze.
- * @returns {Promise<{response: string, fraudDetected: number}>} - An object containing the model's response and a flag indicating potential fraud.
+ * @returns {Promise<{fraudDetected: number, phishing_score: number, result_rationale: string}|null>} -
+ *   An object containing the detection result, including a flag indicating potential fraud,
+ *   phishing score, and result rationale. Returns null in case of error.
  */
 async function detectFraudInEmail(emailContent) {
-
-    // // Prepare the prompt with the system and user messages
-    // const prompt = ChatPromptTemplate.fromMessages([
-    //     ["system", fraudDetectionPrompt],
-    //     ["user", emailContent],
-    // ]);
-
-    // // Initialize the output parser
-    // const outputParser = new StringOutputParser();
-
-    // // Create a processing chain from prompt to model invocation to parsing
-    // const llmChain = prompt.pipe(chatModel).pipe(outputParser);
-
-    // try {
-    //     // Invoke the chain to get the response from the language model
-    //     const response = await llmChain.invoke();
-    //     console.log("Response from language model:", response);
-
-    //     // Determine if the response suggests fraud
-    //     let fraudDetected = response.toLowerCase().includes("fraud") ? 1 : 0;
-
-    //     return { response, fraudDetected };
-    // } catch (error) {
-    //     console.error("Error in detecting fraud:", error);
-    //     throw error; // Rethrow the error to be handled by the caller
-    // }
 
     // Initialize the OpenAI model with the API key
     const OPENAI_API_KEY = process.env.OPENAI_API_KEY; // Ensure your environment variable is configured
@@ -84,7 +60,6 @@ async function detectFraudInEmail(emailContent) {
     })
 
     const runnable = prompt.pipe(model_params).pipe(parser);
-    // const llmChain = prompt.pipe(chatModel).pipe(outputParser);
 
     try {
       const result = await runnable.invoke();
